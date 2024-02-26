74a272b59ed85e175bfd442d6721944fda5b507e
Tontico <antho.noex@live.fr>
2024-02-26 16:41:42 +0100

    modification home

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,16 +6,16 @@ const Header = () => {
     return (
         <>
             <header className="h-24 flex items-center ">
-                <nav className="flex justify-around w-full p-5">
+                <nav className="flex  w-full p-5">
                     <Link href="/home " className="w-1/3"><img src="" alt="logo"></img></Link>
-                    <ul className="flex justify-end w-2/3">
-                        <li className="mr-4">Accueil</li>
-                        <li className="mr-4">Mes projets</li>
-                        <li className="mr-4">Contacts</li>
+                    <ul className="flex justify-evenly w-1/3">
+                        <li className="">Accueil</li>
+                        <li className="">Mes projets</li>
+                        <li className="">Contacts</li>
                     </ul>
                 </nav>
             </header>
-            <div className="border-b border-yellow-700 w-2/3 mx-auto"></div>
+            <div className="border-b border-amber-600 w-96 mx-auto"></div>
         </>
     );
 }
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import { useEffect, useState } from "react";
@@ -13,16 +13,20 @@ export default function Home() {
     <>
       <Header />
       <main className="flex justify-center items-center">
-        <section className="m-5 flex justify-center gap-4 items-center">
+        <section className="m-5 flex justify-center gap-6 items-center">
           <div>
-            <img src="images/Img-portfolio.jpg" className="w-64 h-64 rounded-full border-4 border-yellow-700 "></img>
+            <img src="images/Img-portfolio.jpg" className="w-64 h-64 rounded-full border-2 border-amber-600 "></img>
           </div>
-          <div className="text-center">
-            <p>Bonjour, je suis</p>
-            <h1>Anthony Suraci</h1>
-            <h2>Developpeur Web Full-Stack</h2>
-            <button>Voir le cv</button>
-            <button>En savoir plus sur moi !</button>
+          <div className="flex flex-col gap-4 justify center items-center">
+            <div className="text-center flex gap-1 flex-col justify center items-center">
+              <p>Bonjour, je suis</p>
+              <h1 className="text-4xl text-amber-600">Anthony Suraci</h1>
+              <h2>Developpeur Web Full-Stack</h2>
+            </div>
+            <div className="text-center flex gap-4 justify center items-center">
+              <button className="bg-amber-600 rounded-full p-3">Voir le cv</button>
+              <button className="border rounded-full p-3">En savoir plus !</button>
+            </div>
           </div>
         </section>
       </main>
