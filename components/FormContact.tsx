05924eb4ebfc39f2b20ac7db90020b6c21bfe531
Tontico <antho.noex@live.fr>
2024-03-06 19:01:49 +0100

    modi

--- components/FormContact.tsx
+++ components/FormContact.tsx
@@ -41,7 +41,7 @@ const FormContact = () => {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            await axios.post(`https://nodejs-serverless-function-express-five-xi.vercel.app/api/mailer`, formData);
+            await axios.post(`https://sa-mon-portfolio.vercel.app/mailer`, formData);
             setValidatedLogs(true);
             setFormData({
                 firstname: '',
